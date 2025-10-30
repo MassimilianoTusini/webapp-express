b567be0eeb3521861bdae1f6a330a1efdc3870c5
MassimilianoTusini <massimilianotusini110@gmail.com>
2025-10-30 16:52:44 +0100

    Refactoring app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000;
 const movieRouter = require("./routers/movies");
 const notFound = require("./middlewares/notFound");
 const errorHandler = require("./middlewares/errorHandler");
-const imagePath = require('./middlewares/imagePath');
+const setImagePath = require('./middlewares/imagePath');
 
 // Middleware per leggere JSON
 app.use(express.json());
@@ -12,22 +12,25 @@ app.use(express.json());
 // Middleware per file statici
 app.use(express.static("public"));
 
+// Middleware per gestire i path delle immagini
+app.use(setImagePath);
+
 // Rotta base
 app.get("/", (req, res) => {
-    res.send("<h1>Benvenuto nella Movies API</h1><p>Vai su <a href='/movies'>/movies</a> per vedere la lista dei film.</p>")
+    res.send("<h1>Benvenuto nella Movies API</h1><p>Vai su <a href='/movies'>/movies</a> per vedere la lista dei film.</p>");
 });
 
-app.use("/api/movies", movieRouter);
+// Rotte principali
+app.use("/movies", movieRouter);
 console.log("Router montato su /movies");
 
-// Middleware gestione Path imgs per le rotte
-app.use(imagePath);
-
-// MIddleware per rotte non trovate
+// Middleware per rotte non trovate
 app.use(notFound);
+
+// Middleware gestione errori
 app.use(errorHandler);
 
 // Avvio del server
 app.listen(port, () => {
-    console.log(`Server attivo su http://localhost:${port}`)
-})
+    console.log(`Server attivo su http://localhost:${port}`);
+});
